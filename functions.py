b08f40d28e4137c20b06e907aa66580e7ff7b819
Ersalor <mehmet.z.sanli@gmail.com>
2025-05-22 10:38:00 +0300

    ayırma işlemi yapıldı

---
+++ functions.py
@@ -0,0 +1,24 @@
+#functions
+
+def main():
+    pass
+
+
+def display_Data():
+
+    with open("annual-co2-emissions-per-country.csv","r") as file:
+        count=0
+        for line in file:
+            count+=1
+            parts=line.strip().split(",") #parts[0]=> country name  parts[1]=>country code  parts[2]=>year  parts[3]=>emission
+            country=parts[0]
+            year=parts[2]
+            emission=parts[3]
+            if parts[1]=="":
+                country_Code=None
+
+
+
+
+if __name__=="__main__":
+    main()
